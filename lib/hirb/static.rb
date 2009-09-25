14b970175cb8b88c3ab348cf8aa40f77035dca4d
Justin Camerer <iamjwc@gmail.com>
2009-09-25 16:27:38 -0400

    Removed static hirb file

--- lib/hirb/static.rb
+++
@@ -1,8 +0,0 @@
-require 'rack'
-
-Hirb::Application = Rack::Static.new(Hirb::App,
-  :root => File.join(File.dirname(__FILE__), "..", "..", "public"),
-  :urls => "/")
-
-#Hirb::Application = Hirb::App
-
